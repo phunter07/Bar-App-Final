5b36f565329312deda734c35bff4d2f8d7bd14fb
phunter07 <phunter07@qub.ac.uk>
2015-04-24 14:59:32 +0100

    Register Button

--- Copy of PubCrawlv2_0/gen/com/version1_0/ClubCrawl/R.java
+++ Copy of PubCrawlv2_0/gen/com/version1_0/ClubCrawl/R.java
@@ -2097,9 +2097,10 @@ containing a value of this type.
         public static final int ic_plusone_standard_off_client=0x7f020055;
         public static final int ic_plusone_tall_off_client=0x7f020056;
         public static final int lavs_logo=0x7f020057;
-        public static final int main_background=0x7f020058;
-        public static final int powered_by_google_dark=0x7f020059;
-        public static final int powered_by_google_light=0x7f02005a;
+        public static final int loginbutton=0x7f020058;
+        public static final int main_background=0x7f020059;
+        public static final int powered_by_google_dark=0x7f02005a;
+        public static final int powered_by_google_light=0x7f02005b;
     }
     public static final class id {
         public static final int B1_5hour_activityMain=0x7f0c008f;
